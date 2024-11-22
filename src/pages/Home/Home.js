a03005cba63662bd53ba5cd76e230200ccc9a597
shaxmeHub <shohsuvor976@gmail.com>
2024-11-22 20:46:05 +0600

    add large files

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+// import BannerBottom from "../../components/Banner/BannerBottom";
+import BestSellers from "../../components/home/BestSellers/BestSellers";
+import NewArrivals from "../../components/home/NewArrivals/NewArrivals";
+// import Sale from "../../components/home/Sale/Sale";
+import SpecialOffers from "../../components/home/SpecialOffers/SpecialOffers";
+import YearProduct from "../../components/home/YearProduct/YearProduct";
+
+const Home = () => {
+  return (
+    <div className="w-full mx-auto">
+      <Banner />
+      {/* <BannerBottom /> */}
+      {/*  ============================== Vazvrad / Free delivery */}
+      <div className="max-w-container mx-auto px-4">
+        {/*============================== SALE SKIDKA boganda kere bolad */}
+        {/* <Sale /> */}
+        <NewArrivals />
+        <BestSellers />
+        <YearProduct />
+        <SpecialOffers />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
