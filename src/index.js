921bb1e9560bac094e8b035c097a1f35bf57861a
shaxmeHub <shohsuvor976@gmail.com>
2024-11-22 22:01:25 +0600

    idiot

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import "slick-carousel/slick/slick.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import "./index.css";
-import App from "./App";
+import App from "./App";import { Analytics } from "@vercel/analytics/react"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
+      <Analytics />
     </PersistGate>
   </Provider>
 );
